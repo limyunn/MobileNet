a2f368fb8dcb3eaf695e94c4e3cfdb9730c2b4f9
limyunn <1569233527@qq.com>
2022-05-15 16:08:37 +0800

    model

---
+++ MobileNet v2.py
@@ -0,0 +1,15 @@
+"""MobileNet v2 models for Keras.
+# Reference
+- [Inverted Residuals and Linear Bottlenecks Mobile Networks for
+   Classification, Detection and Segmentation]
+   (https://arxiv.org/abs/1801.04381)
+"""
+
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, GlobalAveragePooling2D, Dropout
+from tensorflow.keras.layers import Activation, BatchNormalization, Add, Reshape, DepthwiseConv2D
+
+
+from tensorflow.keras import backend as K
+
